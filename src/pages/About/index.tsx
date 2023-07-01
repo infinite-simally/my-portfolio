7fc000005d6b827fc14c15e1b96b93d0fb44b5a0
Yana Kortelova <kortelova.work@gmail.com>
2023-07-01 14:25:29 -0600

    feat: [About Page] Added rgb colors, and base for About Page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,15 @@
+import YanaImg from "../../assets/images/yana.png";
+
 import styles from "./About.module.scss";
 
 const AboutPage = () => {
-  return <div className={styles.Container}>ABOUT</div>;
+  return (
+    <div className={styles.Container}>
+      <div className={styles.PortraitContainer}>
+        <img className={styles.Portrait} alt='Yana Kortelova' src={YanaImg} />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
