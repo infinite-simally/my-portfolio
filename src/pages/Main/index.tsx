236fb4115bcb5b8eb87ebc7be7e8be4e004e162f
Yana Kortelova <kortelova.work@gmail.com>
2023-07-31 20:48:22 -0600

    fix: missing bracket

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "context/ThemeContext/ThemeProvider";
 
 import Header from "./components/Header";
