7b0981b98d40573f2d17d2997d1b8d2d03ac001a
Yana Kortelova <kortelova.work@gmail.com>
2023-09-01 20:41:42 -0600

    feat: Migrated from cra to vite

--- src/pages/Main/components/MainAnimation/index.tsx
+++ src/pages/Main/components/MainAnimation/index.tsx
@@ -1,15 +1,21 @@
-import ThreeContainer from "components/ThreeContainer";
+import { Canvas } from "@react-three/fiber";
 import Sphere from "./Spheres/Spheres";
 
 import styles from "./MainAnimation.module.scss";
 
 const MainAnimation = () => {
   return (
-    <ThreeContainer className={styles.CanvasContainer}>
-      <pointLight position={[0, 5, 3]} intensity={0.55} />
-      <ambientLight intensity={0.45} color='#fff' />
-      <Sphere position={[0, 0, 0]} />
-    </ThreeContainer>
+    <div id='canvas-container' className={styles.CanvasContainer}>
+      <Canvas
+        camera={{ fov: 25, position: [0, 0, 12] }}
+        flat={true}
+        dpr={[window.devicePixelRatio, 2]}
+      >
+        <pointLight position={[0, 5, 3]} intensity={0.55} />
+        <ambientLight intensity={0.45} color='#fff' />
+        <Sphere position={[0, 0, 0]} />
+      </Canvas>
+    </div>
   );
 };
 
